7570b3a5c0ea7d612f5b2509e54e3bf95c8b35d0
VishnuK <vishnuskhatale@gmail.com>
2025-09-12 11:53:10 +0000

    auto-commit for d0191aff-7db1-4d08-9725-c81075e8fbd4

--- frontend/src/components/ServiceDelivery.js
+++ frontend/src/components/ServiceDelivery.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
-import { toast } from 'react-hot-toast';
+import { useToast } from '../hooks/use-toast';
 import {
   Building2,
   Calendar,
